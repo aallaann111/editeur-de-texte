955d2b576ced6f32062b9e4073b22421ace7631a
alan terrier <alan.terrier@etu.umontpellier.fr>
2023-11-07 13:35:29 +0100

    Ajout de la classe CommandeRemplacer
    Modification de createCommand

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,26 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument {
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if (parameters.length < 4) {
+            System.err.println("Format attendu : remplacer;depart;fin;chaine");
+            return;
+        }
+
+        int depart = Integer.parseInt(parameters[1]);
+        int fin = Integer.parseInt(parameters[2]);
+        String chaine = parameters[3];
+
+        this.document.remplacer(depart, fin, chaine);
+        super.executer();
+    }
+}
+
